8346616d6c55099df142c190673697cbf899d0a9
Omprakash Maurya <mauryaomprakash2005@gmail.com>
2024-09-19 14:06:52 +0530

    issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
 import Community from "./Components/Community";
 import Navbar from "./Components/Navbar";
@@ -10,14 +8,12 @@ import Footer from "./Components/Footer";
 function App() {
   return (
     <BrowserRouter basename="Deshanta-Vyavaha">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Body />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
