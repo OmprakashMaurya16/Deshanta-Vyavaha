db3c0f40d1945466daf331e1996dddb65a78d751
Omprakash Maurya <mauryaomprakash2005@gmail.com>
2024-09-19 13:46:12 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
 import Community from "./Components/Community";
-import Navbar from "./Components/Navbar"
-import Footer from "./Components/Footer"
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/community" element={<Community />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <BrowserRouter basename="Deshanta-Vyavaha">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </BrowserRouter>
   );
 }
 
