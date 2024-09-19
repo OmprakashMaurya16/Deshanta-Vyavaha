e65c0475a9b9738b34ac09a22cce9ba95d464e0d
Omprakash Maurya <mauryaomprakash2005@gmail.com>
2024-09-19 18:09:03 +0530

    Last Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
 import Community from "./Components/Community";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import "./index.css";
 
 function App() {
   return (
-    <BrowserRouter basename="Deshanta-Vyavaha">
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/community" element={<Community />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'; // Correct import for React 18
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-// Create a root and render your application
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="Deshanta-Vyavaha">
     <App />
   </BrowserRouter>
 );
