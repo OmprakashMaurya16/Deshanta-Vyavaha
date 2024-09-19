47eceec47f1d63825def04090a25aa478146f9b2
Omprakash Maurya <mauryaomprakash2005@gmail.com>
2024-09-19 13:50:50 +0530

    last change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+
 import Body from "./Components/Body";
 import Community from "./Components/Community";
 import Navbar from "./Components/Navbar";
